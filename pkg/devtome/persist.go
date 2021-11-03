773a08e027d4a898bdf22a86ec9487327fc85b6d
Felipe Silva <felipe.dacs@gmail.com>
2021-11-03 01:18:04 -0300

    :new: add estrutura inicial de persistencia

---
+++ pkg/devtome/persist.go
@@ -0,0 +1,41 @@
+package devtome
+
+import (
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type BackupFile struct {
+	Name string
+}
+
+func FilePersist(path string, articles []Article) (err error) {
+	var sb strings.Builder
+
+	for _, a := range articles {
+		sb.WriteString(path)
+		sb.WriteString("/")
+		sb.WriteString(TranslateTitle2Filename(a.Title))
+		sb.WriteString(".md")
+		log.Fatal(sb.String())
+		f, err := os.OpenFile(sb.String(), os.O_CREATE, 0644)
+		if err != nil {
+			return err
+		}
+		// _, err = f.Write([]byte("Hello"))
+		if err != nil {
+			return err
+		}
+		f.Close()
+	}
+
+	return
+}
+
+func TranslateTitle2Filename(origin string) string {
+	s := strings.ToLower(origin)
+	var re = regexp.MustCompile(`\s`)
+	return re.ReplaceAllString(s, "_")
+}
