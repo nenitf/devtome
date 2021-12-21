0564d2634df139bc5b71ffb077ea1999bf70c1a4
Felipe Silva <felipe.dacs@gmail.com>
2021-12-21 12:51:52 -0300

    :100: remove créditos já contidos no README

--- pkg/devtome/client.go
+++ pkg/devtome/client.go
@@ -9,11 +9,6 @@ import (
 	"github.com/pkg/errors"
 )
 
-// THANKS:
-// https://golang.cafe/blog/golang-httptest-example.html
-// https://dev.to/eminetto/testing-apis-in-golang-using-apitest-1860
-// https://pace.dev/blog/2018/05/09/how-I-write-http-services-after-eight-years.html
-
 type Article struct {
 	Id          int    `json:"id"`
 	Title       string `json:"title"`
