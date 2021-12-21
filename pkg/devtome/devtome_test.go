9a67d0de458c2d1f56fe3176cbd288d61f4a0fba
Felipe Silva <felipe.dacs@gmail.com>
2021-12-20 23:46:02 -0300

    :100: unifica _test

--- pkg/devtome/devtome_test.go
+++ pkg/devtome/devtome_test.go
@@ -39,3 +39,28 @@ func TestGetAll(t *testing.T) {
 		is.Equal(strings.TrimSpace(res), string(expected))
 	})
 }
+
+func TestFilePersist(t *testing.T) {
+	tmpdir := t.TempDir()
+
+	p := []devtome.Article{
+		devtome.Article{
+			Id:    1,
+			Title: "Exemplo De Post",
+		},
+	}
+
+	// log.Fatal(tmpdir)
+	devtome.FilePersist(tmpdir, p)
+
+	// tmpdir will be cleaned up
+}
+
+func TestFilename(t *testing.T) {
+	is := is.New(t)
+	o := "Título original do post"
+
+	fn := devtome.TranslateTitle2Filename(o)
+
+	is.Equal(fn, "título_original_do_post")
+}
