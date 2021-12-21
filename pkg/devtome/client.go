a6a0ec488fa273e69892cca99671f7ff54353d26
Felipe Silva <felipe.dacs@gmail.com>
2021-12-21 12:51:06 -0300

    :bug: add envio da key

--- pkg/devtome/client.go
+++ pkg/devtome/client.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/nenitf/devtome/pkg/devto"
 	"github.com/pkg/errors"
 )
 
@@ -30,10 +31,21 @@ func NewClient(url string, token string) Client {
 }
 
 func (c Client) GetAll() ([]Article, error) {
-	res, err := http.Get(c.url + "/api/articles/me/all")
+	client := http.Client{}
+	req, err := http.NewRequest("GET", c.url+"/api/articles/me/all", nil)
 	if err != nil {
 		return []Article{}, errors.Wrap(err, "unable to complete Get request")
 	}
+
+	req.Header = http.Header{
+		"api-key": []string{c.token},
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return []Article{}, errors.Wrap(err, "unable to complete Get request")
+	}
+
 	defer res.Body.Close()
 	out, err := ioutil.ReadAll(res.Body)
 	if err != nil {
